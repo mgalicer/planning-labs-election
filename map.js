2869b007f67f54d1e4676fc8afa770b428e3cc0e
marigalicer <marigalicer@gmail.com>
2018-11-12 21:57:16 -0500

    display red or blue for winning party

--- map.js
+++ map.js
@@ -21,23 +21,39 @@ function initMap() {
     styles: mapStyle
   });
 
-  //load GeoJSON for state polygons
-  map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/states.js', { idPropertyName: 'NAME' });
 
   const selection = document.getElementById('election-year');
+
   google.maps.event.addDomListener(selection, 'change', () => {
     clearData();
-    loadData(selectBox.options[selectBox.selectedIndex].value);
+    loadData(selection.options[selection.selectedIndex].value);
   });
 
+  //load GeoJSON for state polygons
+  map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/states.js', { idPropertyName: 'NAME' });
+}
+
+const styleFeature = (feature) => {
+  var showRow = true;
+  if (feature.getProperty('color') == null) {
+    showRow = false;
+  }
+
+  return {
+    strokeWeight: 0.5,
+    strokeColor: '#fff',
+    fillColor: feature.getProperty('color') ,
+    fillOpacity: 0.75,
+    visible: showRow
+  };
 }
 
 const clearData = () => {
   map.data.forEach((row) => {
     row.setProperty('winner', undefined);
   });
-  document.getElementById('data-box').style.display = 'none';
-  document.getElementById('data-caret').style.display = 'none';
+  // document.getElementById('data-box').style.display = 'none';
+  // document.getElementById('data-caret').style.display = 'none';
 }
 
 const loadData = (url) => {
@@ -49,19 +65,30 @@ const loadData = (url) => {
         let stateName;
         let totalVotes = 0;
         let winningVotes = 0;
-        let winningCandidate = "";
+        let winningCandidate;
 
         for (const candidate of Object.keys(myJSON[state])) {
           stateName = myJSON[state][candidate]['state'];
           let numVotes = myJSON[state][candidate]['votes'];
+
           if(numVotes > winningVotes) {
             winningCandidate = myJSON[state][candidate];
+            winningVotes = numVotes;
           }
           totalVotes += numVotes;
         }
 
-      }
+        let color = '#ff0000';
+        if(winningCandidate['parties'][0] === 'Democratic') color = '#0000ff';
 
+        map.data
+            .getFeatureById(stateName)
+            .setProperty('color', color);
+
+      }
     })
+
+    .then( () => map.data.setStyle(styleFeature))
+
 }
 
