40b6d4cf3396d8ada226f8e61a26ef1550865cd4
marigalicer <marigalicer@gmail.com>
2018-11-13 11:24:53 -0500

    serialize form input to debug element

--- modal.js
+++ modal.js
@@ -1,15 +1,29 @@
-let modal = document.getElementById('user-modal');
+const modal = document.getElementById('user-modal');
 
-let button = document.getElementById("form-button");
+const button = document.getElementById('form-button');
 
 // When the user clicks on the button, open the modal
-button.onclick = function() {
-    modal.style.display = "block";
-}
+button.onclick = () => {
+    modal.style.display = 'block';
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target === modal) {
+        modal.style.display = 'none';
     }
-}
+};
+
+const myForm = document.getElementById('new-candidate-data');
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const object = {};
+
+    new FormData(myForm).forEach((value, key) => {
+        object[key] = value;
+    });
+
+    const json = JSON.stringify(object);
+    document.getElementsByClassName('debug')[0].innerHTML = json;
+});
